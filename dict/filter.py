df3eb3d5da9ef63d42e7590d9ab519e29846ad7c
Antoine Czerwonka <antoine.czerwonka@infopro-digital.com>
2023-02-21 22:44:19 +0100

    update dict/filter.py

--- dict/filter.py
+++ dict/filter.py
@@ -5,11 +5,7 @@ f = open("dictionary.csv", "r")
 
 wordList = []
 
-index = 0
 for mot in f:
-    if (index > 100):
-        pass
-        #break
     mot = mot.strip().upper()
     mot = unidecode(mot)
     if (' ' in mot):
@@ -17,7 +13,6 @@ for mot in f:
     if ('.' in mot):
         continue
     if (len(mot) == 8):
-        index += 1
         wordList.append(mot)
 
 print(json.dumps(wordList))
