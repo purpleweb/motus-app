a422fe6e8aa5a7f229b5264859ed6b96e37bf038
Purpleweb <tonio.cz@gmail.com>
2023-03-01 15:42:17 +0100

    App.test.js test a win

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import * as utils from './utils';
 
 test('renders Motus title', () => {
   render(<App />);
@@ -7,10 +8,16 @@ test('renders Motus title', () => {
   expect(title).toBeInTheDocument();
 });
 
-
 test('play an entire game', () => {
+  utils.generateSolution = jest.fn(() => 'ABRICOTS');
+
   render(<App />);
   fireEvent.click(screen.getByTestId("start"));
   const board = screen.getByTestId('board');
   expect(board).toBeInTheDocument();
+  const input = screen.getByTestId('input');
+  fireEvent.change(input, {target: {value: "ABRICOTS"}});
+  fireEvent.click(screen.getByTestId("valider"));
+  const bravo = screen.getByText(/Bravo/i);
+  expect(bravo).toBeInTheDocument();
 });
--- src/Input.js
+++ src/Input.js
@@ -16,9 +16,9 @@ export function Input({ onWordAdd }) {
         }
     }
 
-    let button = <button className="button is-primary">Valider</button>;
+    let button = <button data-testid="valider" className="button is-primary">Valider</button>;
     if (invalid) {
-        button = <button disabled className="button is-primary">Valider</button>;
+        button = <button data-testid="valider" disabled className="button is-primary">Valider</button>;
     }
 
     return (
@@ -26,7 +26,7 @@ export function Input({ onWordAdd }) {
             <div className="control">
                 <div className="field has-addons">
                     <p className="control">
-                        <input className="input is-focused" maxLength="8" onChange={onInputChange} type="text" name="bid" value={input} />
+                        <input data-testid="input" className="input is-focused" maxLength="8" onChange={onInputChange} type="text" name="bid" value={input} />
                     </p>
                     <p className="control">
                         {button}
