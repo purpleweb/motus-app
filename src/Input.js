24ac5f6504b72ce6644ff1b1ef51da0136f3f0af
Purpleweb <tonio.cz@gmail.com>
2023-03-27 21:13:07 +0200

    Input add const WORD_LENGTH

--- src/Input.js
+++ src/Input.js
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 export function Input({ onWordAdd }) {
     const [input, setInput] = useState('');
-    const invalid = input.length !== 8;
+
+    const WORD_LENGTH = 8;
+    const invalid = input.length !== WORD_LENGTH;
 
     function onInputChange(e) {
         setInput(e.target.value.trim());
@@ -10,7 +12,7 @@ export function Input({ onWordAdd }) {
 
     function handleSubmit(e) {
         e.preventDefault();
-        if (input.length === 8) {
+        if (input.length === WORD_LENGTH) {
             setInput('');
             onWordAdd(input);
         }
@@ -21,7 +23,7 @@ export function Input({ onWordAdd }) {
             <div className="control">
                 <div className="field has-addons">
                     <p className="control">
-                        <input data-testid="input" className="input is-focused" maxLength="8" onChange={onInputChange} type="text" name="bid" value={input} />
+                        <input data-testid="input" className="input is-focused" maxLength={WORD_LENGTH.toString()} onChange={onInputChange} type="text" name="bid" value={input} />
                     </p>
                     <p className="control">
                         <button data-testid="valider" disabled={invalid ? "disabled" : ""} className="button is-primary">Valider</button>
